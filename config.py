f4120551c18bcca4df2525312abd557ba420806c
Ibrahim Mohamed <ibrahimmohamedprof@gmail.com>
2024-10-13 18:04:08 +0000

    config

--- config.py
+++ config.py
@@ -1,12 +1,8 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Config:
-    FLASK_DEBUG=os.getenv('FLASK_DEBUG')
-    FLASK_ENV=os.getenv('FLASK_ENV')
-    FLASK_APP=os.getenv('FLASK_APP')
+    FLASK_DEBUG = os.getenv('FLASK_DEBUG', 'False') == 'True'
+    FLASK_ENV = os.getenv('FLASK_ENV', 'Production')  
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev')
-    SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'postgresql://api_full_user:SsRzf8WjXTmyff2a2EPSQ8e7THnrctFf@dpg-cs5u5i2j1k6c739u9t70-a/api_full')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
